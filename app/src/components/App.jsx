c1e518df0fbb3081ed6b10cd4236f3dfcff4bcab
Aritra Bag <110441011+Aritra-Bag@users.noreply.github.com>
2025-02-04 20:54:30 +0530

    Add files via upload

---
+++ app/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import printHelloWorld from '@/scripts/script';
+import Form from '@/components/Form';
+
+export default function App({ page }) {
+  printHelloWorld();
+  return (
+    <>
+      <Form />
+      </* <img src="/images/background.jpg"/> */></>
+    </>
+  );
+}
